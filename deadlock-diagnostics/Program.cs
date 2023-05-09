cde45513849a941b5fc6ad2d886cbfe38e1ac7a7
Noah Sherwin <noahrsherwin@gmail.com>
2023-05-08 23:10:33 -0700

    refactor: double-check that the lockers' handles closed refactor: assign opened FileStream to variable refactor: define constants, sample file path refactor: write handle locker count to console refactor: write errors to Console.Error, not Trace

--- deadlock-diagnostics/Program.cs
+++ deadlock-diagnostics/Program.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using deadlock_dotnet_sdk;
 using deadlock_dotnet_sdk.Domain;
+using PInvoke;
 using static deadlock_dotnet_sdk.Domain.FileLockerEx.HandlesFilter;
 
 if (!UACHelper.UACHelper.IsElevated)
@@ -10,10 +11,11 @@ if (!UACHelper.UACHelper.IsElevated)
 }
 else
 {
+    const string filePath = @"C:\new.txt";
     Console.WriteLine("Hello, World!");
     Console.WriteLine(@"Opening file handle for C:\new.txt");
     Console.WriteLine("TODO: Why does Console.WriteLine seem to do nothing when called on a worker thread (async)?");
-    File.Open(@"C:\new.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    using FileStream file0 = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
 
     DeadLock? dl = new(false);
     FileLockerEx? fileLockerEx = null;
@@ -21,7 +23,7 @@ else
     var sw = Stopwatch.StartNew();
     try
     {
-        fileLockerEx = dl.FindLockingHandles(filePath: @"C:\new.txt", FilesOnly/*  | IncludeFailedTypeQuery | IncludeNonFiles | IncludeProtectedProcesses */, out WarningException? warningException);
+        fileLockerEx = dl.FindLockingHandles(filePath: filePath, FilesOnly/*  | IncludeFailedTypeQuery | IncludeNonFiles | IncludeProtectedProcesses */, out WarningException? warningException);
         sw.Stop();
         Console.WriteLine(sw.Elapsed);
         if (warningException is not null)
@@ -36,23 +38,47 @@ else
 
     if (fileLockerEx is not null)
     {
-        Console.WriteLine($"{nameof(fileLockerEx)} is not null.");
-        Console.WriteLine(fileLockerEx.Path);
+        const string n_fileLockerEx = nameof(fileLockerEx);
+        const string n_Path = n_fileLockerEx + "." + nameof(fileLockerEx.Path);
+        const string n_Lockers = n_fileLockerEx + "." + nameof(fileLockerEx.Lockers);
+        const string n_Count = n_Lockers + "." + nameof(fileLockerEx.Lockers.Count);
+
+        Console.WriteLine(n_fileLockerEx + " (" + fileLockerEx.GetHashCode() + ") is not null.");
+        Console.WriteLine(n_Path + ": " + fileLockerEx.Path);
+        Console.WriteLine(n_Count + ": " + fileLockerEx.Lockers.Count);
 
         // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation
         foreach (var locker in fileLockerEx.Lockers)
         {
-            Console.WriteLine("Blindly attempting to close the following handle..." + Environment.NewLine + locker);
+            Console.WriteLine("Blindly attempting to close the following handle...\r\n" + locker.ToString());
             bool success = false;
+
             try
             {
-                success = locker.CloseSourceHandle();
+                success = locker.CloseSourceHandle(true);
             }
             catch (Exception ex)
             {
-                Trace.TraceError(ex.ToString());
+                Console.Error.WriteLine(ex.ToString());
             }
+
             Console.WriteLine("Attempt succeeded? [" + success + "]");
+        }
+
+        try
+        {
+            Console.WriteLine("Double-checking the test handles status...");
+
+            var type = Windows.Win32.PInvoke.GetFileType(file0.SafeFileHandle);
+            var err = (Win32ErrorCode)System.Runtime.InteropServices.Marshal.GetLastPInvokeError(); //DevSkim: ignore DS104456 // This DevSkim rule prohibits use of the Marshal class. Why? https://sourcegraph.com/github.com/microsoft/DevSkim@main/-/blob/rules/default/security/control_flow/permission_evelation.json?L29-55
+
+            Console.WriteLine(type is Windows.Win32.Storage.FileSystem.FILE_TYPE.FILE_TYPE_UNKNOWN && err is Win32ErrorCode.ERROR_INVALID_HANDLE
+                ? "The operation succeeded. GetFileType says the handle is invalid/closed."
+                : "The operation failed; " + (err is Win32ErrorCode.ERROR_SUCCESS ? "The handle is still open." : err.GetMessage()));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
     }
 
